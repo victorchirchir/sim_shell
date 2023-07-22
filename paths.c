990e818fce7fedf880a8d84a95022b8a349ca82a
Victor Chirchir <47001153+victorchirchir@users.noreply.github.com>
2023-07-22 23:38:01 +0300

    Create paths.c

---
+++ paths.c
@@ -0,0 +1,118 @@
+#include "holberton.h"
+
+/**
+ * find_path - function that finds the path
+ * @environ: global variable environment
+ * Return: path in tokens
+ */
+
+char **find_path(char **environ)
+{
+	char *get_path, **tokens, *delim;
+
+	delim = ":";
+	get_path = _getenv(environ, "PATH");
+	tokens = splits(get_path, delim);
+	return (tokens);
+}
+
+/**
+ * read_line - reads the command line
+ * Return: line
+ */
+
+char *read_line(void)
+{
+	char *line = NULL;
+	size_t size = 0;
+	int ret = 0;
+
+	if (getline(&line, &size, stdin) == -1)
+	{
+		free(line);
+		exit(-1);
+	}
+	ret = strlen(line);
+	line[ret - 1] = '\0';
+
+	return (line);
+}
+
+/**
+ * _realloc - reallocate memory
+ * @ptr: void variable pointer
+ * @old_size: integer
+ * @new_size: integer
+ * Return: Null
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+
+	char *new;
+	unsigned int i;
+
+	if (ptr == NULL)
+	{
+		new = malloc(new_size);
+		return (new);
+	}
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (new_size == old_size)
+		return (ptr);
+
+	new = malloc(new_size);
+
+	if (new == NULL)
+		return (NULL);
+
+	for (i = 0; i < old_size; i++)
+	{
+		new[i] = ((char *)ptr)[i];
+	}
+
+	free(ptr);
+
+	return (new);
+}
+
+/**
+ * splits - function that create tokens
+ * @line: is a char
+ * @delim: is a char
+ * Return: double pointer
+ */
+
+char **splits(char *line, char *delim)
+{
+	char **pptoken;
+	int buf = 1024, i = 0;
+
+	pptoken = malloc(sizeof(char *) * buf);
+	if (!pptoken)
+		exit(99);
+
+	pptoken[i] = strtok(line, delim);
+	i++;
+	while (1)
+	{
+		pptoken[i] = strtok(NULL, delim);
+		if (i >= buf)
+		{
+			buf += buf;
+			pptoken = _realloc(pptoken, buf, buf * (sizeof(char *)));
+			if (!pptoken)
+				exit(98);
+		}
+		if (pptoken[i] == NULL)
+			break;
+		i++;
+	}
+	return (pptoken);
+}
