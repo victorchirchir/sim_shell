84b3e2cbcca820cb626f8482c0ac3900ac630d13
victorchirchir <vctrchirchir@gmail.com>
2023-07-23 11:31:04 +0000

    added gettingenv

---
+++ gettingenv.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _strncmp-compares one string to another
+ * @str1:string one
+ * @str2:string two
+ * @num:number of chars in prompt
+ * Return: returns 0 if the strings are equal
+ */
+int _strncmp(const char *str1, const char *str2, int num)
+{
+	int i;
+
+	for (i = 0; i < num; i++)
+	{
+		if (str1[i] != str2[i])
+		{
+			return (str1[i] - str2[i]);
+		}
+		else if (str1[i] == '\0' && str2[i] == '\0')
+		{
+			return (0);
+		}
+	}
+	return (0);
+}
+/**
+ * _getenv-gets path in environ
+ * @environ:global variable
+ * @prompt:prompt from user
+ * Return:val or null
+ */
+char *_getenv(char **environ, char *prompt)
+{
+	char *val = NULL;
+	int len, i;
+
+	len = _strlen(prompt);
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		if (_strncmp(environ[i], prompt, len) == 0 && environ[i][len] == '=')
+		{
+			val = environ[i] + len + 1;
+			break;
+		}
+	}
+	return (val);
+}
--- main.h
+++ main.h
@@ -12,6 +12,8 @@
 char *_strdup(char *str);
 char *_strcat(char *destn, char *src);
 char *_strcpy(char *dest, char *src);
+int _strncmp(const char *str1, const char *str2, int num);
+char *_getenv(char **environ, char *prompt);
 int execute(char **parsed);
 char *read_line(void);
 char **splits(char *line, char *delim);
@@ -19,7 +21,6 @@ int _putchar(char ch);
 unsigned int _strlen(char *str);
 void _puts(char *str);
 char **find_path(char **environ);
-char *_getenv(char **environ, char *dirname);
 char *args_path(char **parse, char **tokens);
 char *if_exists(char **environ);
 extern char **environ;
